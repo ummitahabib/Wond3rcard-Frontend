9d693af3845f485de1022709648bbf659ae24147
unknown <ummihabib88@gamil.com>
2025-05-03 11:45:13 -0700

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wond3rcard/app_widget.dart';
 import 'package:wond3rcard/src/utils/storage_utils.dart';
 import 'package:geolocator_web/geolocator_web.dart';
+//prod
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,4 +11,6 @@ Future<void> main() async {
   await StorageUtil.init();
   runApp(const ProviderScope(child: AppWidget()));
 }
+
+
 
